7d341573e825d5cbe2efa81ad02c43f0c54e4c8e
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-22 14:45:14 -0400

    Added code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        System.out.println("Truth table for && operator");
+        System.out.println("true && true = "+ (true&&true));
+        System.out.println("true && false = "+ (true&&false));
+        System.out.println("false && false = "+ (false&&false));
+        System.out.println("false && true = "+ (false&&true));
+        System.out.println("\nTruth table for || operator");
+        System.out.println("true || true = "+ (true||true));
+        System.out.println("true || false = "+ (true||false));
+        System.out.println("false || false = "+ (false||false));
+        System.out.println("false || true = "+ (false||true));
     }
 }
